f1a0dbd1c995b139bb3dd5b6613dab74b67c427e
leonardo zequini <zequini.leonardo@gmail.com>
2022-06-21 18:42:27 -0300

    read

--- crudemjava/src/br/com/agenda/dao/ContatoDAO.java
+++ crudemjava/src/br/com/agenda/dao/ContatoDAO.java
@@ -2,6 +2,9 @@ package br.com.agenda.dao;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mysql.jdbc.PreparedStatement;
 
@@ -18,7 +21,10 @@ public class ContatoDAO {
 	 * d delete
 	 */
 	
+
 	public void save(Contato contato) {
+
+
 		String sql = "INSERT INTO contatos(nome, idade, datacadastro) VALUES(?, ?, ?)";
 		
 		Connection conn = null;
@@ -58,4 +64,60 @@ public class ContatoDAO {
 			}
 		}
 	}
+	
+	
+	public List<Contato> getContatos(){
+		
+		String sql = "SELECT * FROM contatos";
+		
+		List<Contato> contatos = new ArrayList<Contato>();
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		//class que vai recuperar os dados do banco ***SELECT***
+		ResultSet rset = null;
+		
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			rset = pstm.executeQuery();
+			
+			while (rset.next()) {
+				Contato contato = new Contato();
+				
+				//recuperar o id
+				contato.setId(rset.getInt("id"));
+				//recuperar o nome
+				contato.setNome(rset.getString("nome"));
+				//recuperar idade
+				contato.setIdade(rset.getInt("idade"));
+				//recuperar data de cadastro
+				contato.setDataCadastro(rset.getDate("datacadastro"));
+				
+				contatos.add(contato);
+			}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					if(rset!=null) {
+						rset.close();
+					}
+					
+					if(pstm!=null) {
+						pstm.close();
+					}
+					
+					if(conn!=null) {
+						conn.close();
+					}
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return contatos;
+		}	
 }
